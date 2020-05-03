68dd4d0aceb2acaf577e8a1a775c94b2340dd384
Sridhar G K <gksridhar09@gmail.com>
2020-05-03 06:13:26 +0530

    Basic UI Structing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Checkout from "./Checkout";
+import Header from "./Header";
 
-function App() {
-  return <div></div>;
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="main-app">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={Checkout} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
